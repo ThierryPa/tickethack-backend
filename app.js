b4d294e32e3dc47a5ece055d89f9f3772d93dae6
ThierryPa <papouchado.thierry@gmail.com>
2024-07-02 13:00:03 +0200

    Updated packages / files

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+require("dotenv").config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('./models/connection');
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bookingsRouter = require('./routes/bookings');
+var cartsRouter = require('./routes/carts');
+var tripsRouter = require('./routes/trips');
+
 
 var app = express();
+
+const cors = require('cors');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/bookings', bookingsRouter);
+app.use('/carts', cartsRouter);
+app.use('/trips', tripsRouter);
 
 module.exports = app;
