3f03ec895dda12c3c811143533ff03630df6416b
ThierryPa <papouchado.thierry@gmail.com>
2024-07-03 10:12:07 +0200

    Fixed Date on trip.js and added Moment to the package

--- routes/trips.js
+++ routes/trips.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const moment = require('moment');
+
 const Trip = require('../models/trips');
 
-// GET => Chercher une liste de trips avec departure et arrival
-// TODO Ajouter le parametre Date aussi
+// GET => Chercher une liste de trips avec departure, arrival et
+// (date compris entre le debut et la fin de la journée)
 router.get('/:departure/:arrival/:date', (req, res) => {
     const myDate = new Date(req.params.date);
-    Trip.find({ departure: req.params.departure, arrival: req.params.arrival, date: myDate }).then(trips => {
+    const minDate = moment(myDate).startOf('day');
+    const maxDate = moment(myDate).endOf('day');
+    
+    Trip.find({ departure: req.params.departure, arrival: req.params.arrival, date: { $gte: minDate, $lte: maxDate } }).then(trips => {
         // Si la requête n'est pas null ou vide
         if (trips != null && trips != "") {
             console.log(trips);
